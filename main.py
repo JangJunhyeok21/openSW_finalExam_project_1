10fd26fab83e9ab6d7c37361d2c2ca79a845c04a
JangJunhyeok21 <jangjh1995@naver.com>
2021-12-11 13:13:13 +0900

    자료 정리 및 필요한 값 도출하여 시각화

--- main.py
+++ main.py
@@ -8,14 +8,46 @@ pd.set_option('mode.chained_assignment',  None)
 plt.rcParams['font.family'] = 'D2Coding'
 
 #csv파일 불러오기 및 필터링
-accident = pd.read_csv("data//accident.csv", encoding = 'cp949')
-accident['사고율']=accident['사고건수']/accident['등록대수']
-accident['사망률']=accident['사망자수']/accident['사고건수']
-accident4graph=accident.drop(['사망자수','중상자수','경상자수','부상신고자수','등록대수','사고건수'],axis=1)
+accident = pd.read_csv("data//도로교통공단_가해운전자 차종별 피해운전자 차종별 교통사고 통계_20201231.csv", encoding = 'cp949')
+bikeresist=pd.read_csv("data//이륜차신고현황_연도별_20211210003715.csv", encoding = 'cp949')
+carresisttemp=pd.read_csv("data//자동차등록대수현황_연도별_20211205232951.csv", encoding = 'cp949')
+
+#가해차종이 이륜차, 승용차가 아니면 drop 후 하나로 합침
+accidentcar=accident[(accident.가해당사자종=='승용차')].drop('가해당사자종',axis=1)
+inflictaccidentcar=pd.DataFrame({'차종':['승용차'],'종류':['가해'],'사고건수':[accidentcar['사고건수'].sum()],'사망자수':[accidentcar['사망자수'].sum()],'중상자수':[accidentcar['중상자수'].sum()],'경상자수':[accidentcar['경상자수'].sum()]})
+accidentbike=accident[(accident.가해당사자종=='이륜차')].drop('가해당사자종',axis=1)
+inflictaccidentbike=pd.DataFrame({'차종':['이륜차'],'종류':['가해'],'사고건수':[accidentbike['사고건수'].sum()],'사망자수':[accidentbike['사망자수'].sum()],'중상자수':[accidentbike['중상자수'].sum()],'경상자수':[accidentbike['경상자수'].sum()]})
+inflictaccident=pd.concat([inflictaccidentbike,inflictaccidentcar], ignore_index=True)
+
+#피해차종 이하동문
+accidentcar=accident[(accident.피해당사자종=='승용차')].drop('피해당사자종',axis=1)
+damageaccidentcar=pd.DataFrame({'차종':['승용차'],'종류':['피해'],'사고건수':[accidentcar['사고건수'].sum()],'사망자수':[accidentcar['사망자수'].sum()],'중상자수':[accidentcar['중상자수'].sum()],'경상자수':[accidentcar['경상자수'].sum()]})
+accidentbike=accident[(accident.피해당사자종=='이륜차')].drop('피해당사자종',axis=1)
+damageaccidentbike=pd.DataFrame({'차종':['이륜차'],'종류':['피해'],'사고건수':[accidentbike['사고건수'].sum()],'사망자수':[accidentbike['사망자수'].sum()],'중상자수':[accidentbike['중상자수'].sum()],'경상자수':[accidentbike['경상자수'].sum()]})
+damageaccident=pd.concat([damageaccidentbike,damageaccidentcar], ignore_index=True)
+#피해 및 가해 병합
+organizedAccident=pd.concat([inflictaccident,damageaccident], ignore_index=True)
+#이륜차 등록대수 현황 필터링
+bikeresist=bikeresist[bikeresist.시점=='2020']
+bikeresist['차종']='이륜차'
+bikeresist.rename(columns = {'총합계' : '등록대수'}, inplace = True)
+#승용차 등록대수 현황 필터링
+carresisttemp=carresisttemp[carresisttemp['레벨01(1)']=='승용']
+carresist=pd.DataFrame({'차종':['승용차'],'시점':[2020],'등록대수':[carresisttemp['2020.2'].sum()]})
+#등록대수 병합
+resist=pd.concat([carresist,bikeresist], ignore_index=True)
+#등록대수 현황 dataframe과 사고 dataframe 병합, 문자열을 숫자로 자료형 변환
+accidentAndResist=pd.merge(organizedAccident,resist)
+accidentAndResist['등록대수']=pd.to_numeric(accidentAndResist['등록대수'])
+
+#사고율 계산 및 추가
+accidentAndResist['사고율']=accidentAndResist['사고건수']/accidentAndResist['등록대수']
+accidentAndResist['사망률']=accidentAndResist['사망자수']/accidentAndResist['사고건수']
+accident4graph=accidentAndResist.drop(['사망자수','중상자수','경상자수','등록대수','사고건수'],axis=1)
 print(accident4graph)
 accident_pivot=accident4graph.pivot(index='종류',columns='차종',values=['사고율','사망률'])
 accident_pivot.plot.bar(rot=0)
 print("===================================")
-print(accident)
+print(accidentAndResist)
 
 plt.show()
--- raw2organize.py
+++ raw2organize.py
@@ -9,30 +9,43 @@ plt.rcParams['font.family'] = 'D2Coding'
 
 #csv파일 불러오기 및 필터링
 accident = pd.read_csv("data//도로교통공단_가해운전자 차종별 피해운전자 차종별 교통사고 통계_20201231.csv", encoding = 'cp949')
+bikeresist=pd.read_csv("data//이륜차신고현황_연도별_20211210003715.csv", encoding = 'cp949')
+carresisttemp=pd.read_csv("data//자동차등록대수현황_연도별_20211205232951.csv", encoding = 'cp949')
 
 #가해차종이 이륜차, 승용차가 아니면 drop 후 하나로 합침
-accidentcar=accident[(accident.가해당사자종=='승용차')].drop('피해당사자종',axis=1)
-inflictaccidentcar=pd.DataFrame({'차종':['승용차'],'종류':['피해'],'사고건수':[accidentcar['사고건수'].sum()],'사망자수':[accidentcar['사망자수'].sum()],'중상자수':[accidentcar['중상자수'].sum()],'경상자수':[accidentcar['경상자수'].sum()]})
-
-accidentbike=accident[(accident.가해당사자종=='이륜차')].drop('피해당사자종',axis=1)
-inflictaccidentbike=pd.DataFrame({'차종':['이륜차'],'종류':['피해'],'사고건수':[accidentbike['사고건수'].sum()],'사망자수':[accidentbike['사망자수'].sum()],'중상자수':[accidentbike['중상자수'].sum()],'경상자수':[accidentbike['경상자수'].sum()]})
-
+accidentcar=accident[(accident.가해당사자종=='승용차')].drop('가해당사자종',axis=1)
+inflictaccidentcar=pd.DataFrame({'차종':['승용차'],'종류':['가해'],'사고건수':[accidentcar['사고건수'].sum()],'사망자수':[accidentcar['사망자수'].sum()],'중상자수':[accidentcar['중상자수'].sum()],'경상자수':[accidentcar['경상자수'].sum()]})
+accidentbike=accident[(accident.가해당사자종=='이륜차')].drop('가해당사자종',axis=1)
+inflictaccidentbike=pd.DataFrame({'차종':['이륜차'],'종류':['가해'],'사고건수':[accidentbike['사고건수'].sum()],'사망자수':[accidentbike['사망자수'].sum()],'중상자수':[accidentbike['중상자수'].sum()],'경상자수':[accidentbike['경상자수'].sum()]})
 inflictaccident=pd.concat([inflictaccidentbike,inflictaccidentcar], ignore_index=True)
+
 #피해차종 이하동문
-accidentcar=accident[(accident.피해당사자종=='승용차')].drop('가해당사자종',axis=1)
+accidentcar=accident[(accident.피해당사자종=='승용차')].drop('피해당사자종',axis=1)
 damageaccidentcar=pd.DataFrame({'차종':['승용차'],'종류':['피해'],'사고건수':[accidentcar['사고건수'].sum()],'사망자수':[accidentcar['사망자수'].sum()],'중상자수':[accidentcar['중상자수'].sum()],'경상자수':[accidentcar['경상자수'].sum()]})
-
 accidentbike=accident[(accident.피해당사자종=='이륜차')].drop('피해당사자종',axis=1)
-damageaccidentbike=pd.DataFrame({'차종':['이륜차'],'종류':['가해'],'사고건수':[accidentbike['사고건수'].sum()],'사망자수':[accidentbike['사망자수'].sum()],'중상자수':[accidentbike['중상자수'].sum()],'경상자수':[accidentbike['경상자수'].sum()]})
-
+damageaccidentbike=pd.DataFrame({'차종':['이륜차'],'종류':['피해'],'사고건수':[accidentbike['사고건수'].sum()],'사망자수':[accidentbike['사망자수'].sum()],'중상자수':[accidentbike['중상자수'].sum()],'경상자수':[accidentbike['경상자수'].sum()]})
 damageaccident=pd.concat([damageaccidentbike,damageaccidentcar], ignore_index=True)
-
+#피해 및 가해 병합
 organizedAccident=pd.concat([inflictaccident,damageaccident], ignore_index=True)
-
+#이륜차 등록대수 현황 필터링
+bikeresist=bikeresist[bikeresist.시점=='2020']
+bikeresist['차종']='이륜차'
+bikeresist.rename(columns = {'총합계' : '등록대수'}, inplace = True)
+#승용차 등록대수 현황 필터링
+carresisttemp=carresisttemp[carresisttemp['레벨01(1)']=='승용']
+carresist=pd.DataFrame({'차종':['승용차'],'시점':[2020],'등록대수':[carresisttemp['2020.2'].sum()]})
+#등록대수 병합
+resist=pd.concat([carresist,bikeresist], ignore_index=True)
+#등록대수 현황 dataframe과 사고 dataframe 병합
+accidentAndResist=pd.merge(organizedAccident,resist)
+accidentAndResist['등록대수']=pd.to_numeric(accidentAndResist['등록대수'])
 
 print(accident)
-print(accidentcar)
-print(accidentbike)
 print(inflictaccident)
 print(damageaccident)
 print(organizedAccident)
+print('================')
+print(resist)
+print('================')
+print(accidentAndResist)
+print(accidentAndResist.dtypes)
